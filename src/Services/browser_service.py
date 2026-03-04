61b0565a6c5c81642ac5894f6ffdc069b83197b4
Galozi-Rafael <rafael.hist@yahoo.com.br>
2026-03-04 17:21:01 -0300

    feat: Adiciona funções ao browser_service.

--- src/Services/browser_service.py
+++ src/Services/browser_service.py
@@ -1,4 +1,4 @@
-from playwright.sync_api import sync_playwright
+from playwright.sync_api import sync_playwright, Page
 
 # Classe responsável por gerenciar o navegador usando Playwright
 class BrowserService:
@@ -10,7 +10,7 @@ class BrowserService:
         self.context = None
         self.page = None
 
-    def start_browser(self):
+    def start_browser(self) -> Page:
         # Inicia o motor do Playwright
         self.playwright = sync_playwright().start()
         # Lança o navegador chromium
@@ -22,6 +22,18 @@ class BrowserService:
         # Retorna a página já pronta para uso
         return self.page
     
+    def goto(self, url):
+        # Navega para a URL especificada
+        self.page.goto(url)
+
+    def click(self, selector):
+        # Clica no elemento identificado pelo seletor
+        self.page.locator.click(selector)
+
+    def count_elements(self, selector) -> int:
+        # Conta o número de elementos que correspondem ao seletor
+        return self.page.locator(selector).count()
+
     def close_browser(self):
         # Fecha o navegador e o motor do Playwright
         self.context.close()
---
+++ src/main.py
@@ -0,0 +1,8 @@
+from Tests.test_01_abtest import run_TC01
+
+def main():
+
+    run_TC01()
+    
+if __name__ == "__main__":
+    main()
