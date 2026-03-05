864a7c5dee69575564825e875e1c4dd121e86d92
Galozi-Rafael <rafael.hist@yahoo.com.br>
2026-03-04 23:30:59 -0300

    feat: Implementa o TC03 ao main.

--- src/main.py
+++ src/main.py
@@ -1,11 +1,14 @@
 import Tests.test_01_abtest as TC01
 import Tests.test_02_add_elements as TC02
+import Tests.test_03_basic_auth as TC03 
 
 def main():
 
     TC01.run()
 
     TC02.run()
+
+    TC03.run()
 
 if __name__ == "__main__":
     main()
