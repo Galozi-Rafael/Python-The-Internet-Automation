b2aa3db25d616321d4ef3de6fd4f1509285f87ff
Galozi-Rafael <rafael.hist@yahoo.com.br>
2026-03-04 23:30:24 -0300

    feat: Cria a lógica para o TC03 Basic Auth.

---
+++ src/Tests/test_03_basic_auth.py
@@ -0,0 +1,21 @@
+from Services.browser_service import BrowserService
+
+def run():
+
+    browser = BrowserService(headless=False, username="admin", password="admin")
+
+    page = browser.start_browser()
+
+    page.goto("https://the-internet.herokuapp.com/basic_auth")
+
+    # Obtém o texto do heading da página
+    message = page.locator("p").inner_text()
+
+    print(f"O texto da mensagem é: {message}")
+
+    if "Congratulations" in message:
+        print("Teste passou: A mensagem de sucesso foi exibida.")
+    else:
+        print("Teste falhou: A mensagem de sucesso não foi exibida.")
+    
+    browser.close_browser()
