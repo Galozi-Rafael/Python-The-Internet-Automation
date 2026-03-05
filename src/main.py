b01b62c863c6bc7dfc8c8ca999b14a0b64b60b16
Galozi-Rafael <rafael.hist@yahoo.com.br>
2026-03-05 09:56:55 -0300

    feat: Implementa o TC04 ao main.

--- src/main.py
+++ src/main.py
@@ -1,6 +1,7 @@
 import Tests.test_01_abtest as TC01
 import Tests.test_02_add_elements as TC02
 import Tests.test_03_basic_auth as TC03 
+import Tests.test_04_broken_images as TC04
 
 def main():
 
@@ -9,6 +10,8 @@ def main():
     TC02.run()
 
     TC03.run()
+
+    TC04.run()
 
 if __name__ == "__main__":
     main()
