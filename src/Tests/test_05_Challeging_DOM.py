c17b6982b9ca46e71d6f0f6eaf07a1c590249cc5
Galozi-Rafael <rafael.hist@yahoo.com.br>
2026-03-05 22:51:55 -0300

    feat: Cira a lógica para o TC05 Challenging DOM.

---
+++ src/Tests/test_05_Challeging_DOM.py
@@ -0,0 +1,36 @@
+from Services.browser_service import BrowserService
+from Services.excel_service import ExcelService
+from Services.scraper_service import ScraperService
+from Services.canvas_service import CanvasService
+
+def run():
+
+    browser = BrowserService(headless=False)
+    page = browser.start_browser()
+
+    CanvasService.inject_canvas_listener(page)
+
+    page.goto("https://the-internet.herokuapp.com/challenging_dom")
+
+    canvas_text = CanvasService.get_canvas_text(page)
+    print(f"Texto capturado do canvas: {canvas_text}")
+
+    page.click(".button")
+
+    page.click(".button.alert")
+
+    page.click(".button.success")
+
+    table_data = ScraperService.extract_table(page)
+
+    excel_service = ExcelService()
+
+    excel_service.save_to_excel(table_data, "challenging_dom_table.xlsx")
+
+    browser.close_browser()
+
+
+
+
+
+
