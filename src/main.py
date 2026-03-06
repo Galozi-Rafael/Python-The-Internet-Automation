c6b715201791bd6e3bc50784d1ba045504e965a6
Galozi-Rafael <rafael.hist@yahoo.com.br>
2026-03-05 22:53:15 -0300

    feat: Implementa o TC05 ao main.

--- src/main.py
+++ src/main.py
@@ -2,16 +2,20 @@ import Tests.test_01_abtest as TC01
 import Tests.test_02_add_elements as TC02
 import Tests.test_03_basic_auth as TC03 
 import Tests.test_04_broken_images as TC04
+import Tests.test_05_Challeging_DOM as TC05
+
 
 def main():
 
-    TC01.run()
+    #TC01.run()
 
-    TC02.run()
+    #TC02.run()
 
-    TC03.run()
+    #TC03.run()
 
-    TC04.run()
+    #TC04.run()
+
+    TC05.run()
 
 if __name__ == "__main__":
     main()
