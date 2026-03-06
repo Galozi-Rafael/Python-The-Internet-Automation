62f7e619f1cd833fafa98ab3c74f513591783316
Galozi-Rafael <rafael.hist@yahoo.com.br>
2026-03-06 08:57:25 -0300

    feat: Implementa o TC08 ao main.

--- src/main.py
+++ src/main.py
@@ -3,19 +3,22 @@ import Tests.test_02_add_elements as TC02
 import Tests.test_03_basic_auth as TC03 
 import Tests.test_04_broken_images as TC04
 import Tests.test_05_Challeging_DOM as TC05
+import Tests.test_08_digest_auth as TC08
 
 
 def main():
 
-    #TC01.run()
+    TC01.run()
 
-    #TC02.run()
+    TC02.run()
 
-    #TC03.run()
+    TC03.run()
 
-    #TC04.run()
+    TC04.run()
 
     TC05.run()
+
+    TC08.run()
 
 if __name__ == "__main__":
     main()
