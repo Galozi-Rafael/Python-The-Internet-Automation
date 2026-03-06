bb2863286cd76203a3701f9a379e4315560dea5a
Galozi-Rafael <rafael.hist@yahoo.com.br>
2026-03-05 22:01:39 -0300

    feat: Cria a lógica para o excel_service.

---
+++ src/Services/excel_service.py
@@ -0,0 +1,14 @@
+import pandas as pd
+
+# Serviço para salvar dados em um arquivo Excel
+class ExcelService:
+
+    # Método para salvar dados em um arquivo Excel
+    @staticmethod
+    def save_to_excel(self, data, file_name):
+
+        df = pd.DataFrame(data)
+
+        df.to_excel(file_name, index=False)
+
+        print(f"Tabela salva em {file_name} com sucesso!")
